eb3f4a977e358043692a5f505313a2aea1162ca9
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-04-01 15:48:58 -0300

    Inicia Banner

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
-import {Container, Nav, Navbar} from 'react-bootstrap'
+import {Button, Container, Nav, Navbar} from 'react-bootstrap'
 import Logo from './assets/img/logo.jpg'
+import Banner from './assets/img/banner.jpg'
 
 
 function Header () {
@@ -26,7 +27,7 @@ function Header () {
 
 function Footer () {
   return (
-    <footer className='footer text-center text-black py-2 mt-4'>
+    <footer className='footer text-center text-white py-2 mt-4'>
       <p className='m-0'>Todos os direitos reservados a Dark-Kitchens.</p>
     </footer>
   )
@@ -36,9 +37,17 @@ function HomeView () {
   return (
     <>
       <Header/>
-      <main>
-        <p>Conteúdo</p>
-      </main>
+        <main>
+          <Container>
+            <div className='shadow border p-4 my-3 banner-home'>
+              <h1>Bem vindo(a) a Dark-Kitchens!</h1>  
+              <p>Conheça nossas cozinhas.</p>
+              <p>Contrate a sua agora mesmo.</p>
+              <Button className='text-uppercase'color='waring'>Kitchens</Button>
+              <img src={Banner} alt='Dark-Kitchens' width={626} height={391} className='img-fluid' />
+            </div>
+          </Container>
+        </main>
       <Footer/>
    </>
   )
