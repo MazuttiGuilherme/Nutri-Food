9414cd47c41d3c68020d772bb9889719826a06fe
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-04-01 14:14:43 -0300

    implementa o header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,41 @@
-function App() {
+import {Container, Nav, Navbar} from 'react-bootstrap'
+import Logo from './assets/img/logo.jpg'
+
+
+function Header () {
   return (
-    <p>Portal de Cursos</p>
-  );
+    <header>
+      <Navbar bg='warning' expand='md' className='main-navbar'>
+        <Container>
+          <Navbar.Brand href='/'>
+          <img src={Logo} alt='Dark-Kitchens'/>
+          </Navbar.Brand>
+          <Navbar.Toggle 
+          aria-controls='navbar-header'/>
+          <Navbar.Collapse id='navbar-header'>
+              <Nav className='ms-auto'>
+                <Nav.Link href='/'>Home</Nav.Link>
+                <Nav.Link href='/kitchens'>Kitchens</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+        </Container> 
+    </Navbar>
+  </header>
+  )
+}
+
+function HomeView () {
+  return (
+    <>
+      <Header/>
+      <p>Conteúdo</p>
+      <p>Rodapé</p>
+    </> 
+  )
+}
+
+function App() {
+  return <HomeView/>
 }
 
 export default App;
