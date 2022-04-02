86e84ef063dc0ac1ec1c7b6e072c47e360f590fd
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-04-01 21:52:23 -0300

    Termina página 404

--- front-end/src/views/NotFound/index.js
+++ front-end/src/views/NotFound/index.js
@@ -1,9 +1,14 @@
+import { Container } from "react-bootstrap"
 import { Layout } from "../../components/Layout"
 
 export function NotFoundView () {
     return (
       <Layout>
-        <h1>Página não encontrada</h1>
+        <Container>
+          <h1>Página não encontrada</h1>
+          <p>A página que você está procurando não existe ou foi movida.</p>
+          <p>Utilize o menu principal para navegar entre as páginas.</p>
+        </Container>
       </Layout>
     )
   }
