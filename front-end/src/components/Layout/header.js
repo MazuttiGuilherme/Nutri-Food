e80f75a2251cfe937d9156414d493c3ca0b0c7f7
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-04-01 21:47:22 -0300

    Instala React Router Dom

--- front-end/src/components/Layout/header.js
+++ front-end/src/components/Layout/header.js
@@ -1,10 +1,10 @@
 import { Container, Nav, Navbar} from 'react-bootstrap'
-import Logo from './assets/img/logo.jpg'
+import Logo from '../../assets/img/logo.jpg'
 
 export function Header () {
     return (
       <header>
-        <Navbar bg='warning' expand='md' className='main-navbar'>
+        <Navbar expand='md' className='main-navbar'>
           <Container>
                 <Navbar.Brand href='/'>
                     <img src={Logo} alt='Dark-Kitchens' width={181} height={181}/>
@@ -13,8 +13,8 @@ export function Header () {
                     aria-controls='navbar-header'/>
                 <Navbar.Collapse id='navbar-header'>
                 <Nav className='ms-auto'>
-                    <Nav.Link href='/'>Home</Nav.Link>
-                    <Nav.Link href='/kitchens'>Kitchens</Nav.Link>
+                    <Nav.Link href='/'>HOME</Nav.Link>
+                    <Nav.Link href='/kitchens'>KITCHENS</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
           </Container> 
--- front-end/src/components/Layout/index.js
+++ front-end/src/components/Layout/index.js
@@ -1,5 +1,5 @@
-import { Header } from './header' 
-import Footer from './footer'
+import { Header } from './Header' 
+import Footer from './Footer'
 
 export function Layout ({children}) {
     return (
