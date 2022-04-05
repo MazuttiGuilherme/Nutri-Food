9270998a6781b795fe4c5dbf52f265b12df8f5d6
guilherme_mazutti <guilherme.mazutti@datagrupo.com.br>
2022-04-05 11:21:16 -0300

    Gerando Build

--- front-end/src/components/Layout/Footer.js
+++ front-end/src/components/Layout/Footer.js
@@ -1,6 +1,8 @@
 export default function Footer () {
     return (
-      <footer className='footer text-center text-white py-2 mt-4'>
+      <footer 
+      className='footer text-center text-white py-2 mt-4'
+      >
         <p className='m-0'>Todos os direitos reservados a Dark-Kitchens.</p>
       </footer>
     )
