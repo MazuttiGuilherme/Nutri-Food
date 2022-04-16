10fa5edacd3aaa68dc6f3b9e426d8106e542ec0a
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-04-16 16:31:44 -0300

    Finaliza primeira parte do projeto

--- front-end/src/views/KtchenDetail/index.js
+++ front-end/src/views/KtchenDetail/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Alert, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { Layout } from "../../components/Layout";
@@ -11,28 +11,28 @@ export function KitchenDetailView() {
   const { id } = useParams();
   const [kitchen, setKitchen] = useState();
   const [loading, setLoading] = useState(true);
-  const [errorMsg, setErrorMsg] = useState();
+  const [errorMsg, setErrorMsg] = useState()
+  const fetchKitchen = useCallback(async () => {
+    try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/
+    kitchens/${id}?_embed=inscriptions`);
+    if (!response.ok) {
+        throw new Error('Response not ok.')
+    }
+    const data = await response.json();
+    setKitchen(data);
+    setLoading(false);
+  } catch (err){
+      const message = err.message === 'Response not ok.'
+      ? '404' 
+      :'Falha ao buscar informações do curso. Recarregue a página.'
+      setErrorMsg(message)
+      setLoading(false)
+  }
+}, [id] )
   useEffect(() => {
-    const fetchKitchen = async () => {
-      try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/
-        kitchens/${id}?_embed=inscriptions`);
-        if (!response.ok) {
-            throw new Error('Response not ok.')
-        }
-        const data = await response.json();
-        setKitchen(data);
-        setLoading(false);
-      } catch (err){
-          const message = err.message === 'Response not ok.'
-          ? '404' 
-          :'Falha ao buscar informações do curso. Recarregue a página.'
-          setErrorMsg(message)
-          setLoading(false)
-      }
-    };
     fetchKitchen();
-  }, [id]);
+  }, [fetchKitchen]);
   if (loading) {
     return <Loading />;
   }
@@ -41,7 +41,7 @@ export function KitchenDetailView() {
   }
   return (
     <Layout>
-        <Container className="course-detail-container">
+        <Container className="kitchen-detail-container">
             {errorMsg ? (
                 <Alert variant="danger" className="mt-3">{errorMsg}</Alert>
             ) :(
@@ -49,8 +49,7 @@ export function KitchenDetailView() {
             <h1>{kitchen?.name}</h1>
             <p>{kitchen.description}</p>
             <Inscriptions inscriptions={kitchen.inscriptions} />
-            <InscriptionForm />
-
+            <InscriptionForm kitchenId={id} onRegister= {fetchKitchen} />
         </>
             )}
         </Container>
--- front-end/src/views/KtchenDetail/inscriptionForm.js
+++ front-end/src/views/KtchenDetail/inscriptionForm.js
@@ -1,8 +1,17 @@
 import { useState } from "react"
-import { Button, Form } from "react-bootstrap"
+import { Alert, Button, Form } from "react-bootstrap"
+
+const initialFormData = {
+    userName: '',
+    userEmail: ''
+
+}
 
 
-export function InscriptionForm () {
+export function InscriptionForm (courseId, onRegister) {
+    const [showSuccess, setShowSuccess] = useState(false)
+    const [isSubmiting, setIsSubmiting] = useState(false)
+    const [errorMsg, setErrorMsg] = useState()
     const [formData, setFormData] = useState({
         userName: '',
         userEmail:''
@@ -13,11 +22,40 @@ export function InscriptionForm () {
             [event.target.name]: event.target.value
         })
     }
-   
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try {
+        setErrorMsg(undefined)
+        setIsSubmiting(true)
+          await fetch(`${process.env.REACT_APP_API_URL}/inscriptions`,{
+                method:'POST',
+                body:JSON.stringify({
+                    name: formData.userName,
+                    email:formData.userEmail,
+                    courseId: parseInt(courseId)
+                }),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            setShowSuccess(true)
+            setFormData(initialFormData)
+            onRegister()
+        } catch (err) {
+            setErrorMsg('Falha ao fazer inscrição. Tente novamente.')
+        }
+        setIsSubmiting(false)
+    }
     return (
         <>
         <h2>Formulário de Inscrição</h2>
-        <Form>
+        {showSuccess && (
+            <Alert variant="success" dismissible onClose={() => setShowSuccess(false)}>Inscrito com sucesso.</Alert>
+        )}
+        {errorMsg && (
+            <Alert variant="danger">{errorMsg}</Alert>
+        )}
+        <Form onSubmit={handleSubmit}>
             <Form.Group controlId="inscription-name" className="mb-3">
                 <Form.Label className="m-0">Nome</Form.Label>
                 <Form.Control
@@ -25,18 +63,21 @@ export function InscriptionForm () {
                     value={formData.userName}
                     onChange={handleChange}
                     name="userName"
+                    required
                 />
             </Form.Group>
             <Form.Group controlId="inscription-email" className="mb-3">
                 <Form.Label className="m-0">E-mail</Form.Label>
                 <Form.Control
+                    type='email'
                     placeholder="exemplo@exemplo.com"
                     value={formData.userEmail}
                     onChange={handleChange}
                     name="userEmail"
+                    required
                     />
                 </Form.Group>
-                <Button type='submit'>Inscrever</Button>
+                <Button type='submit' disabled={isSubmiting}>Inscrever</Button>
             </Form> 
         </>
     )
