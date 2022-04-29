577e13f6e0cde0bdbdcfe152abee4af1ce95afc5
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-04-29 14:49:24 -0300

    Salva dados do usuário no storage

--- front-end/src/services/Users.service.js
+++ front-end/src/services/Users.service.js
@@ -1,4 +1,5 @@
 import { apiUrl } from "./Api.service"
+import { setStorageItem } from "./Storage.service"
 
 export const login = async (credentialsData) => {
     const response = await fetch(`${apiUrl}/login`, {
@@ -8,11 +9,16 @@ export const login = async (credentialsData) => {
             'content-type': 'application/json'
         }
     })
+    const data = await response.json()
     if (!response.ok){
-        const data = await response.json()
         const message = data === 'Incorrect password' || data === 'Cannot find user'
           ? 'Credentials invalid.'
           : 'Response not ok.'
         throw new Error(message)
     }
+    const userData = {
+        accessToken: data.accessToken,
+        ...data.user
+    }
+    setStorageItem('user', JSON.stringify(userData))
 }
