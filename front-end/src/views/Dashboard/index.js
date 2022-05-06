c09ebdcf7e4e10e26092582b6cf723e99c3d6279
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-05-06 12:12:18 -0300

    Exibe dashboard de acordo com usuário

--- front-end/src/views/Dashboard/index.js
+++ front-end/src/views/Dashboard/index.js
@@ -1,12 +1,18 @@
 import { Alert } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { LayoutPortal } from "../../components/LayoutPortal";
+import { selectUser } from "../../store/User/User.selectors";
 
 export function DashboardView () {
+  const user = useSelector(selectUser)
     return (
     <LayoutPortal>
-      <h1 className="mt-4">Bem vindo(a) Guilherme.</h1>
-      <p>Utilize o menu para gerenciar os dados do site.</p>
+      <h1 className="mt-4">Bem vindo(a) {user.name}</h1>
+      {user.type === 1 ? (
+        <p>Utilize o menu para gerenciar os dados do site.</p>
+      ) : (
       <Alert variant="info">Você receberá todas as informações do Plano por e-mail</Alert>
+      )}
     </LayoutPortal>
   )
 }
