36a16c34f2d8eebcfb5165f2fbc4285e0e7e306a
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-05-06 12:56:30 -0300

    Exibindo menus conforme tipo de usuário

--- front-end/src/components/LayoutPortal/Sidebar.js
+++ front-end/src/components/LayoutPortal/Sidebar.js
@@ -1,19 +1,24 @@
 import { CloseButton, Nav } from "react-bootstrap";
 import styled from "styled-components";
 import { SidebarItem } from "./SidebarItem";
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../store/User/User.selectors'
 
 const menuItems = [
   {
     to: "/portal",
     text: "Dashboard",
+    userTypes: [1, 2]
   },
   {
     to: "/portal/kitchens",
     text: "Planos",
+    userTypes: [1]
   },
 ];
 
 export function Sidebar({ isOpen, onClose }) {
+  const { type } = useSelector(selectUser) 
   return (
     <SidebarStyled
       className="bg-dark text-white d-flex 
@@ -28,7 +33,7 @@ export function Sidebar({ isOpen, onClose }) {
       <p className="h1">Nutri-Food</p>
       <hr />
       <Nav variant="pills" className="flex-column">
-        {menuItems.map((item, index) => (
+        {menuItems.filter(item => item.userTypes.includes(type)).map((item, index) => (
           <SidebarItem key={index} item={item}/>
         ))}
       </Nav>
